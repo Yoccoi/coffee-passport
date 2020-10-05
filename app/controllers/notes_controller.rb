6ebc0772f0b3f7afd48c4c8f1faf0c759a17a6d1
Yoccoi <ba023082@gmail.com>
2020-10-05 22:39:45 +0900

    Active Storageの導入

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,4 +2,24 @@ class NotesController < ApplicationController
   def index
   end
 
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(note_params)
+    if @note.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def note_params
+    params.permit(:name, :rating, :comment, :image, :bitter, :acidity, :aroma, :body, :roast_date, :grams, :price).merge(user_id: current_user.id)
+  end
+
 end
