c74f55562231eaa99b147225386f71a5938f9529
Yoccoi <ba023082@gmail.com>
2020-10-11 14:08:25 +0900

    Shopテーブル&コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'notes#index'
   resources :notes
+  resources :shop, only: [:new, :create, :index, :show, :edit]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_05_132749) do
+ActiveRecord::Schema.define(version: 2020_10_09_024537) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -50,6 +50,16 @@ ActiveRecord::Schema.define(version: 2020_10_05_132749) do
     t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
+  create_table "shops", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.date "purchase_date"
+    t.text "location"
+    t.bigint "note_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["note_id"], name: "index_shops_on_note_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -66,4 +76,5 @@ ActiveRecord::Schema.define(version: 2020_10_05_132749) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "notes", "users"
+  add_foreign_key "shops", "notes"
 end
