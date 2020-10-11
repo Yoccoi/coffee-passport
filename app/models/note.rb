e93636808d203c60283c0309b14eef6f1352b896
Yoccoi <ba023082@gmail.com>
2020-10-11 14:10:59 +0900

    shopモデルとのアソシエーション

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,7 +1,8 @@
 class Note < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  has_one :shop
 
   validates :name, presence: true
-  validates :comment, presence: true
+  validates :rating, presence: true
 end
