0c095cd073fe507265b6601678f7c86f3f04567c
Yoccoi <ba023082@gmail.com>
2020-10-17 19:18:50 +0900

    記録の新規作成＆詳細ページ編集

--- app/javascript/popup.js
+++ app/javascript/popup.js
@@ -1,13 +1,15 @@
-    $(function() {
-        var popup = $('#popup')
-        $(window).scroll(function() {
-            if ($(this).scrollTop() > 300) {
-                popup.fadeIn();
-            } else {
-                popup.hide();
-            }
-        });
-        $('button').click(function() {
-            $('#popup').hide();
-        });
+$(function() {
+    var popup = $('#popup');
+    var flag = true; // （コードは書いていないけど）ローカルストレージにでも保存しておけばいい
+    $(window).scroll(function() {
+        if (flag && $(this).scrollTop() > 300) {
+            popup.fadeIn();
+        } else {
+            popup.hide();
+        }
     });
+    $('button').click(function() {
+        flag = false; // （コードは書いていないけど）ローカルストレージにでも保存しておけばいい
+        $('#popup').hide();
+    });
+});
